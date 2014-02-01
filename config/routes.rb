756f954c6daa1f80041dc9345a37774d661e3328
Seth Dowden <sethclarkdowden@gmail.com>
2014-02-01 15:09:43 -0800

    Controler index install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Flutter::Application.routes.draw do
+  root "index#index"
+  get "index/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
